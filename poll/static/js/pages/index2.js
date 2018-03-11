0f1cc44772e2d73f63ee762738f29de7d45f5a39
urlguy <urlguy7@gmail.com>
2018-03-11 14:35:27 +0545

    with graphics

---
+++ poll/static/js/pages/index2.js
@@ -0,0 +1,72 @@
+﻿$(function () {
+    //Widgets count
+    $('.count-to').countTo();
+
+    //Sales count to
+    $('.sales-count-to').countTo({
+        formatter: function (value, options) {
+            return '$' + value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, ' ').replace('.', ',');
+        }
+    });
+
+    initRealTimeChart();
+    initDonutChart();
+    initSparkline();
+});
+
+var realtime = 'off';
+function initRealTimeChart() {
+    //Real time ==========================================================================================
+    var plot = $.plot('#real_time_chart', [getData()], {
+        series: {
+            shadowSize: 0,
+            color: 'rgb(0, 188, 212)'
+        },
+        grid: {
+            borderColor: '#f3f3f3',
+            borderWidth: 1,
+            tickColor: '#f3f3f3'
+        },
+        lines: {
+            fill: true
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        xaxis: {
+            min: 0,
+            max: 9,
+        }
+    });
+
+    function updateRealTime() {
+        plot.setData([getData()]);
+        plot.draw();
+
+        var timeout;
+        if (realtime === 'on') {
+            timeout = setTimeout(updateRealTime, 320);
+        } else {
+            clearTimeout(timeout);
+        }
+    }
+
+    updateRealTime();
+
+    $('#realtime').on('change', function () {
+        realtime = this.checked ? 'on' : 'off';
+        updateRealTime();
+    });
+    //====================================================================================================
+}
+
+function initSparkline() {
+    $(".sparkline").each(function () {
+        var $this = $(this);
+        $this.sparkline('html', $this.data());
+    });
+}
+
+
+
